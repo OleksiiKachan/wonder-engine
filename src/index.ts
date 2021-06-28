dd691d5c9f2c938cbdcc7775e28bf70790575f6f
OleksiiKachan <okachan@nobul.com>
2021-06-28 19:04:37 -0400

    feat: create LinkContainer
    create component that render Link or component based on props

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './context';
 
 export { default as Portal } from './Portal';
 export { default as BaseButton } from './base-button';
+export { default as LinkContainer } from './link-container';
