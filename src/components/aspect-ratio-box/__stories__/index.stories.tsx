25ef3da3180438b22e5dedd855df514894c0d1a7
OleksiiKachan <okachan@nobul.com>
2021-07-24 14:58:03 -0400

    refactor: move components to components/
    move to components/
    init AspectRatioBox
    fix AspectRatioBox types
    fix AspectRationBox ratio order

---
+++ src/components/aspect-ratio-box/__stories__/index.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import AspectRatioBox from '../index';
+
+export default {
+  title: 'Components/AspectRatioBox',
+  component: AspectRatioBox,
+  argTypes: {
+    type: { control: 'select', options: ['1:1', '16:9'] },
+  },
+} as ComponentMeta<typeof AspectRatioBox>;
+
+export const Template: ComponentStory<typeof AspectRatioBox> = (args) => (
+  <AspectRatioBox
+    {...args}
+    style={{ backgroundColor: 'red', width: '300px' }}
+  />
+);
+
+// export const Primary = Template.bind({});
+// Primary.args = {
+//   primary: true,
+//   label: 'Button',
+// };
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as AspectRatioBox } from './aspect-ratio-box';
+export { default as BaseButton } from './base-button';
+export { default as LinkContainer } from './link-container';
