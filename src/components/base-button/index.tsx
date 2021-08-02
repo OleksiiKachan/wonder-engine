f1351267f2c9c36dd5d7717dee5f86cd2466415f
OleksiiKachan <okachan@nobul.com>
2021-08-02 13:05:38 -0400

    refactor: change ref structure

--- src/components/base-button/index.tsx
+++ src/components/base-button/index.tsx
@@ -17,57 +17,59 @@ interface BaseButton
     disabled?: boolean;
     loading?: boolean;
     onClick: (event: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => void;
+    forwardRef: any;
   }> {}
 
-const BaseButton: BaseButton = forwardRef(
-  (
-    {
-      component,
-      href,
-      target,
-      rel,
-      tabIndex = 0,
-      disabled = false,
-      loading = false,
-      onClick = () => {},
-      children,
-      ...otherProps
+const BaseButton: BaseButton = ({
+  component,
+  href,
+  target,
+  rel,
+  tabIndex = 0,
+  disabled = false,
+  loading = false,
+  onClick = () => {},
+  children,
+  forwardRef,
+  ...otherProps
+}) => {
+  const commonProps: object = {
+    disabled,
+    onClick: (event: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
+      if (!loading) {
+        onClick(event);
+      }
     },
-    ref
-  ) => {
-    const commonProps: object = {
-      disabled,
-      onClick: (event: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
-        if (!loading) {
-          onClick(event);
-        }
-      },
-      tabIndex: !loading && !disabled ? tabIndex : -1,
-      ref,
-    };
+    tabIndex: !loading && !disabled ? tabIndex : -1,
+    ref: forwardRef,
+  };
 
-    let props: object = {};
+  let props: object = {};
 
-    let linkProps = { href, target, rel };
+  let linkProps = { href, target, rel };
 
-    if (component) {
-      props = {
-        ...props,
-        ...linkProps,
-      };
-    } else if (href) {
-      props = { ...linkProps };
-    }
-
-    const buttonProps = {
-      ...commonProps,
+  if (component) {
+    props = {
       ...props,
-      ...otherProps,
-      component: component || 'button',
+      ...linkProps,
     };
-
-    return <LinkContainer {...buttonProps}>{children}</LinkContainer>;
+  } else if (href) {
+    props = { ...linkProps };
   }
-);
 
-export default BaseButton;
+  const buttonProps = {
+    ...commonProps,
+    ...props,
+    ...otherProps,
+    component: component || 'button',
+  };
+
+  return <LinkContainer {...buttonProps}>{children}</LinkContainer>;
+};
+
+const Forwarded = forwardRef<any>((props: any, ref) => (
+  <BaseButton forwardedRef={ref} {...props} />
+));
+Forwarded.displayName = 'BaseButton';
+
+export default Forwarded;
