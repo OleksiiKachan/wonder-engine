248aa5a67a3fc700c6caf7cfa9a0288177649ead
OleksiiKachan <okachan@nobul.com>
2021-09-06 11:06:42 -0400

    docs(storybook): add button story

---
+++ src/components/base-button/__stories__/index.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button from '../index';
+import { WonderEngineProvider } from '../../../context';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    href: { control: 'text', defaultValue: null },
+    disabled: { control: 'boolean', defaultValue: false },
+    loading: { control: 'boolean', defaultValue: false },
+  },
+} as ComponentMeta<typeof Button>;
+
+export const Template: ComponentStory<typeof Button> = (args) => (
+  <WonderEngineProvider config={{ Link: 'a' }}>
+    <Button {...args}>Submit</Button>
+  </WonderEngineProvider>
+);
