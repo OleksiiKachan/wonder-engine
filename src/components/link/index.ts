437d467de72df261ef83f6f7649b39ab8322663c
OleksiiKachan <okachan@nobul.com>
2022-10-23 18:40:01 -0400

    feat(elements): add Link component

---
+++ src/components/link/index.ts
@@ -0,0 +1,38 @@
+import { createElement, forwardRef, useMemo } from 'react';
+
+import { useWonderEngineContext } from '../../context';
+import { generateProps } from '../link-container';
+
+/**
+ * Types imports
+ */ import type { FunctionComponent } from 'react';
+import {
+  LinkContainerProps,
+  LinkContainerRefAttributes,
+} from '../link-container';
+
+/**
+ * Types
+ */
+export interface Link
+  extends FunctionComponent<
+    Omit<LinkContainerProps, 'component'> & LinkContainerRefAttributes
+  > {}
+
+const Link: Link = forwardRef(({ children, ...otherProps }, ref) => {
+  const { Link: LinkComponent } = useWonderEngineContext();
+
+  const commonProps: object = {
+    ref,
+  };
+
+  const props = useMemo(() => generateProps(otherProps), [otherProps]);
+
+  return createElement(
+    LinkComponent,
+    { ...commonProps, ...props, ...otherProps },
+    children
+  );
+});
+
+export default Link;
